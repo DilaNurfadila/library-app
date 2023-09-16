d5e390dff78211308dacb347df6037d2f53150d2
DilaNurfadila <nurfadila1523@gmail.com>
2023-09-16 21:03:50 +0700

    Create frontend (HTML CSS) & edit static file

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require("path");
+// const path = require("path");
 
-app.use(express.static(path.join(__dirname), "public"));
+// app.use(express.static(path.join(__dirname), "public"));
+app.use(express.static("public"));
 
 // Get root (/)
 // root = https://localhost:[port]
 app.get("/", (req, res) => {
-  //   res.send("Hello World"); // This text will display on page
+  // res.send("Hello World"); // This text will display on page
   res.get("index.html");
 });
 
 // Listen and display message in console
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
